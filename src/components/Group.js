b5586954f7c4c256eb979af0f3eaee189060ae35
Mihail Dubrovksy <43237977+MikeArvalor@users.noreply.github.com>
2021-10-01 12:35:10 +0300

    CRM_mobile

--- src/components/Group.js
+++ src/components/Group.js
@@ -1,7 +1,11 @@
 import * as React from 'react';
 import { View, Text, Button,StyleSheet, ImageBackground,TouchableOpacity, ScrollView,Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+
 //import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import SelectDropdown from 'react-native-select-dropdown'
+
 import {
   createDrawerNavigator,
   DrawerContentScrollView,
@@ -9,57 +13,53 @@ import {
   DrawerItem,
   
 } from '@react-navigation/drawer';
-
+const subject = ["Python", "QA", "Frond-end"]
 function Home({ navigation }) {
 
   return (
     <ScrollView style={styles.scrollView}>
     <View style={styles.firstBlock}>
-    
-     <ImageBackground source={require('./assets/Group.png')} resizeMode="cover" style={styles.image}>
-       <Text style={styles.text}>
-         Группы
-       </Text>
-     </ImageBackground>
-     <ImageBackground source={require('./assets/raspisaniy.png')} resizeMode="cover" style={styles.image}>
-       <Text style={styles.text}>
-         Расписание
-       </Text>
-     </ImageBackground>
-     <ImageBackground source={require('./assets/teachers.png')} resizeMode="cover" style={styles.image}>
-       <Text style={styles.text}>
-         Преподаватели
-       </Text>
-     </ImageBackground>
-     <ImageBackground source={require('./assets/lid.png')} resizeMode="cover" style={styles.image}>
-       <Text style={styles.text}>
-         Лиды
-       </Text>
-     </ImageBackground>
-     <View style={styles.newsBlock}>
-       <View style={styles.headerNews} >
-         <Text style={styles.headerNewsText}>Наши новости</Text>
-       </View>
-       <View style={styles.underNews}>
-          <View style={styles.underNewsText}>
-            <Text style={styles.underNewsText1}>Подвинься, Tiguan? Прячьтесь, конкуренты? Тест-драйв нового Volkswagen Taos</Text>
-            <Text style={styles.underNewsText2}>В этом году новые модели кроссоверов словно боровики и подосиновики — появляются едва ли не ежедневно. Но особенно тесно становится на «поляне» компактных SUV....</Text>
+      <View style={styles.firstBlockLeft}>
+        <Text style={styles.text1}>Группы</Text>
+        <SelectDropdown
+          data={subject}
+          onSelect={(selectedItem, index) => {
+            console.log(selectedItem, index)
+          }}
+          buttonTextAfterSelection={(selectedItem, index) => {
+            return selectedItem
+          }}
+          rowTextForSelection={(item, index) => {
+            return item
+          }}
+        />
+        </View> 
+        <View style={styles.firstBlockRight}>
+          <Text style={styles.text1}>Поиск по номеру группы</Text>
+          <SelectDropdown
+          style={styles.selectdown}
+          width='100'
+          data={subject}
+          onSelect={(selectedItem, index) => {
+            console.log(selectedItem, index)
+          }}
+          buttonTextAfterSelection={(selectedItem, index) => {
+            return selectedItem
+          }}
+          rowTextForSelection={(item, index) => {
+            return item
+          }}
+        />
+        </View>
+        <View style={styles.secondBlock}>
+          <View >
+            <Text style={styles.titleBlock}>Python</Text>
           </View>
-          <TouchableOpacity
-          style={styles.button}>
-          <Text>Смотреть</Text>
-      </TouchableOpacity>
-           <View style={styles.underNewsText}>
-            <Text style={styles.underNewsText1}>Налоговая занялась маршрутками: пассажиры без билетов и загадочный рост выручки</Text>
-            <Text style={styles.underNewsText2}>Управление оперативных мероприятий инспекции Министерства по налогам и сборам в своем телеграм-канале сообщило о начале проверке маршрутных такси. «Маршрут 1280-ТК (ДС „Уручье-4“....</Text>
+          <View style={styles.cardSecondBlock}>
           </View>
-          <TouchableOpacity
-          style={styles.button}>
-          <Text>Смотреть</Text>
-          </TouchableOpacity>
-       </View>
-     </View>
+        </View>   
     </View>
+    
     </ScrollView>
     
   );
@@ -121,13 +121,7 @@ function CustomDrawerContent(props) {
     </DrawerContentScrollView>
   );
 }
-function img(){
-  return(
-    <View>
-      <Image source={require('./assets/logo.png')}/>
-    </View>
-  )
-}
+
 const Drawer = createDrawerNavigator();
 
 function MyDrawer() {
@@ -150,71 +144,38 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  newsBlock:{
-    display:"flex",
-    justifyContent:"center",
-    alignItems:"center"
-  },
-  underNewsText:{
-    textAlign:"left",
-    justifyContent:"flex-start",
-    alignItems:"flex-start",
-    backgroundColor:"#F3E9EC",
-    marginBottom:20
-  },
-  
-  underNewsText1:{
-  textAlign:"left",
-  fontWeight:"bold",
-  fontSize:24,
-  padding:5,
-
-  justifyContent:"flex-end"  
-},
-underNewsText2:{
-  textAlign:"left",
-padding:5,
-  fontSize:20,
-  justifyContent:"flex-end",
-  marginBottom:10
-},
-  headerNewsText:{
-    display:"flex",
-    justifyContent:"center",
-    alignItems:"center",
-    textAlign:"center",
-    fontSize:24,
-    marginBottom:20,
-    marginTop:10
-  },
-  headerNews:{
-    display:"flex",
-    justifyContent:"center",
-    alignItems:"center",
-    textAlign:"center",
  
-  },
   firstBlock:{
     display:"flex",
     flex:1,
     flexDirection:"row",
     justifyContent:"space-between",
     flexWrap:"wrap",
-    margin:10
-    
+   
   },
-  image: {
-    width:190,
-    height:190,
-    justifyContent: "center",
+  secondBlock:{
+    marginTop:20,
+    width:'100%',
+    backgroundColor:"#D5E8D4",
+  },
+  titleBlock:{
+    fontSize:20,
+    marginTop:20,
     marginBottom:20,
-    backgroundColor: "#000000c0"
+    paddingLeft:10
+    
   },
   button: {
     alignItems: "center",
     backgroundColor: "#DDDDDD",
     padding: 10,
     marginBottom:20
+  },
+  text1:{
+  marginBottom:20,
+  marginTop:20,
+  fontSize:16,
+  paddingLeft:10
   },
   text: {
     color: "white",
